d72263b4308c723d48e77116cd58b138704b7c15
USER <user@USERs-MacBook-Pro.local>
2025-07-13 15:50:49 +0100

    creating the layouts

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Home({name }) {
     return (
         <>
-            <h1 className="title">Welcome Home</h1>
+            <h1 className="title">Welcome {name}</h1>
         </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return inertia('Home');
+    return Inertia::render('Home', ['name' => "Olajide"]);
 });
+
+
+// Route::get('/', [PostController::class, 'index']);
+
+// Route::resource('posts', PostController::class)->except('index');
