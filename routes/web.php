a8633b1deb67cc6ad5f595b8bdb0946f513cc1c9
USER <user@USERs-MacBook-Pro.local>
2025-07-13 16:22:45 +0100

    preparing the models and resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    sleep(2);
-    return Inertia::render('Home', ['name' => "Olajide"]);
-});
 
 
-// Route::get('/', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
 
-// Route::resource('posts', PostController::class)->except('index');
+Route::resource('posts', PostController::class)->except('index');
