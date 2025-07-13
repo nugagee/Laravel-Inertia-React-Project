8543f7313baabb7375283afe83cac6db53943695
USER <user@USERs-MacBook-Pro.local>
2025-07-13 16:51:31 +0100

    creating dummy data and rendering on homepage and listing resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return inertia('Home');
+        // $posts = Post::all();
+        $posts = Post::latest()->get();
+        return inertia('Home', ['posts' => $posts]);
     }
 
     /**
