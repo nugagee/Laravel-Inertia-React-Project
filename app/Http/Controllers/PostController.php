899fbcdcfd5c9fdf0b619e35f90ccbf9a4a4d0b6
USER <user@USERs-MacBook-Pro.local>
2025-07-15 15:45:23 +0100

    deleting a post or resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -71,6 +71,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        // dd($post);
+        $post->delete();
+
+        return redirect('/');
     }
 }
--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -2,14 +2,14 @@ import { Link, useForm } from "@inertiajs/react";
 import { useRoute } from "../../../vendor/tightenco/ziggy";
 
 export default function Show({ post }) {
-    // const { delete: destroy } = useForm();
+    const { delete: destroy } = useForm();
     const route = useRoute();
 
-    // function submit(e) {
-    //     e.preventDefault();
-    //     // destroy(`/posts/${post.id}`);
-    //     destroy(route("posts.destroy", post));
-    // }
+    function submit(e) {
+        e.preventDefault();
+        // destroy(`/posts/${post.id}`);
+        destroy(route("posts.destroy", post));
+    }
 
     return (
         <>
@@ -22,17 +22,17 @@ export default function Show({ post }) {
                 </div>
                 <p className="font-medium">{post.body}</p>
 
-                {/* <div className="flex items-center justify-end gap-2">
+                <div className="flex items-center justify-end gap-2">
                     <form onSubmit={submit}>
                         <button className="bg-red-500 rounded-md text-sm px-4 py-1 text-white">
                             Delete
                         </button>
                     </form>
 
-                    <Link href={`/posts/${post.id}/edit`} className="bg-green-500 rounded-md text-sm px-4 py-1 text-white">Update</Link>
+                    {/* <Link href={`/posts/${post.id}/edit`} className="bg-green-500 rounded-md text-sm px-4 py-1 text-white">Update</Link> */}
 
-                    <Link href={route('posts.edit', post)} className="bg-green-500 rounded-md text-sm px-4 py-1 text-white">Update</Link>
-                </div> */}
+                    {/* <Link href={route('posts.edit', post)} className="bg-green-500 rounded-md text-sm px-4 py-1 text-white">Update</Link> */}
+                </div>
             </div>
         </>
     );
