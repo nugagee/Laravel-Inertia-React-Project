4da2602bfc76afbd6c043419cca27a5442b77ac9
USER <user@USERs-MacBook-Pro.local>
2025-07-15 15:18:51 +0100

    creating a resources and new post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -24,15 +23,23 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        // dd($request);
+
+        $fields = $request->validate([
+            'body' => ['required']
+        ]);
+
+        Post::create($fields);
+
+        return redirect('/');
     }
 
     /**
@@ -54,7 +61,7 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
         //
     }
